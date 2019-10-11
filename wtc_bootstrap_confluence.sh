dcf5cae50d5c8d656a6744fa81685538e0a1224a
Britt Thompson <BThompson@1path.com>
2019-10-11 14:57:10 -0400

    confluence bootstrap

---
+++ wtc_bootstrap_confluence.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#----------------- Import variables
+if [ -e "wtc_env.sh" ]; then
+  . wtc_env.sh
+fi
+
+#----------------- Install applications
+export DEBIAN_FRONTEND=noninteractive
+apt update -y
+apt install -y docker.io python3 python3-pip certbot awscli git ssl-cert jq monit
+
+
+#----------------- Enable the Docker daemon
+systemctl start docker
+systemctl enable docker
+
+
+#----------------- Upgrade the awscli
+yes | pip3 install --upgrade pip
+yes | pip install awscli --upgrade --user
+
+
+#----------------- Set the hostname to the primary site URL
+hostnamectl set-hostname $SITE_URL
+sed -i "s/127.0.0.1 localhost/127.0.0.1 localhost $SITE_URL/g" /etc/hosts
+sed -i "s/$HOSTNAME/$SITE_URL/g" /etc/hostname
+sed -i "s/preserve_hostname: true/preserve_hostname: false/g" /etc/cloud/cloud.cfg
+
+
+#----------------- Make all the required directories
+mkdir -p ${CONFLUENCE_HOME}
+mkdir -p /etc/mysql/connector
+mkdir -p /root/.aws
+
+
+#----------------- Create the aws credentials file and config
+{
+  echo "[default]"
+  echo "aws_access_key_id = ${AWS_ACCESS_KEY}"
+  echo "aws_secret_access_key = ${AWS_SECRET_KEY}"
+} > /root/.aws/credentials
+
+cat > /root/.aws/config <<-'EOF'
+[default]
+output = json
+region = us-east-1
+EOF
+
+
+#----------------- Authenticate to the AWS Elastic Container Registry
+$(aws ecr get-login --no-include-email --region us-east-1)
+
+
+#----------------- Get the MySQL Drivers
+curl -sLOH 'Cache-Control: no-cache' 'https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.48.tar.gz' -o 'mysql-connector.tar.gz';
+tar -xvf mysql-connector.tar.gz -C /etc/mysql/connector;
+
+
+#----------------- Launch docker containers
+docker run --network host --name proftpd --restart always -e PROFTPD_MASQUERADE_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4) -v /var/www:/var/www -td ${DOCKER_REGISTRY}/proftpd
+#docker run --network host --name postfix --restart always -d ${DOCKER_REGISTRY}/postfix
+docker run --name confluence --restart always -p 8090:8090 -p 8091:8091 -v ${CONFLUENCE_HOME}:/var/atlassian/application-data/confluence -v /etc/mysql/connector/mysql-connector-java-5.1.48.jar:/opt/atlassian/confluence/lib/mysql-connector-java-5.1.48.jar -d atlassian/confluence-server
